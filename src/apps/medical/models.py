b7a7058ec9b88f2b5bbbf0e317afbd429664f605
leethobbit <dking8921@gmail.com>
2024-03-13 19:25:15 -0400

    Added medical models, updated service names in compose yaml

--- src/apps/medical/models.py
+++ src/apps/medical/models.py
@@ -1,34 +1,30 @@
 from django.db import models
 
+from apps.animals.models import Animal
+
+EVENT_CHOICES = [
+    ("WELLNESS / VACCINE", "Wellness / Vaccine"),
+    ("SURGERY", "Surgery"),
+    ("DIAGNOSIS", "Diagnosis"),
+    ("MEDICATION / TREATMENT", "Medication / Treatment"),
+]
 # Create your models here.
-
-
-# class Treatment
 class Treatment(models.Model):
-
-    name = models.CharField(max_length=80, null=False, unique=True)
+    name = models.CharField(max_length=80, null=False)
     description = models.TextField(blank=True, null=False, default="")
-    cost = models.DecimalField(max_digits=6, decimal_places=2, default=5.00)
-
-    class Meta:
-        db_table_comment = "Holds information about kinds of treatments."
-
+    
     def __str__(self):
         return self.name
 
-
-# class Vaccine
-class Vaccine(models.Model):
-
+class MedicalRecord(models.Model):
     name = models.CharField(max_length=80, null=False, unique=True)
+    animal = models.ForeignKey(Animal, on_delete=models.PROTECT, null=False)
+    event_type = models.CharField(max_length=80, choices=EVENT_CHOICES)
     description = models.TextField(blank=True, null=False, default="")
-    cost = models.DecimalField(max_digits=6, decimal_places=2, default=5.00)
+    needs_treatment = models.BooleanField(default=False)
 
     class Meta:
-        db_table_comment = "Holds information about kinds of vaccines."
+        db_table_comment = "Table holds medical record entries."
 
     def __str__(self):
         return self.name
-
-
-# class Diagnosis
