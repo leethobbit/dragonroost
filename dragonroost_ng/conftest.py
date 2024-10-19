18213c0b53a706b2457123658ffdfb6e2976618b
Dan <47875749+leethobbit@users.noreply.github.com>
2024-10-19 12:26:09 -0400

    Initial commit after cookiecutter and config

---
+++ dragonroost_ng/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from dragonroost_ng.users.models import User
+from dragonroost_ng.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
